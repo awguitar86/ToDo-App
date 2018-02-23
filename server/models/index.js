878ec9d312933ec9edf60cbb5e20dd1ec67a81ab
Austin Wright <awguitar86@gmail.com>
2018-02-23 12:12:26 -0700

    changes

--- server/models/index.js
+++ server/models/index.js
@@ -13,7 +13,7 @@ let sequelize;
 if (config.use_env_variable) {
   //From the environment, extract the key with the name provided in the config as use_env_variable
   //and use that to establish a connection to our database.
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config
