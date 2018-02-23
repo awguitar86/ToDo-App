942001c54064b4ff0af3d8774935cfc64372760c
Austin Wright <awguitar86@gmail.com>
2018-02-23 12:23:22 -0700

    changes

--- .server/models/index.js
+++ .server/models/index.js
@@ -5,7 +5,7 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '../../server/config/config.json')[env];
+var config    = require('../../server/config/config.json')[env];
 var db        = {};
 
 let sequelize;
