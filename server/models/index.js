c0c25ee58ecd9bf4c923c6161c53180253a56df0
Austin Wright <awguitar86@gmail.com>
2018-02-23 11:10:28 -0700

    changes

--- server/models/index.js
+++ server/models/index.js
@@ -8,19 +8,24 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../../server/config/config.json')[env];
 var db        = {};
 
+let sequelize;
+
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
 }
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
+  .filter(file =>
+    (file.indexOf('.') !== 0) &&
+    (file !== basename) &&
+    (file.slice(-3) === '.js'))
   .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
