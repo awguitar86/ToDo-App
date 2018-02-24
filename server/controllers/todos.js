da86f3bbd87fab7a2b412e5f7bdb52529166c166
Austin Wright <awguitar86@gmail.com>
2018-02-24 09:35:34 -0700

    wrote code to fetch all todos from database

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,12 +1,19 @@
 const Todo = require('../../.server/models').Todo;
 
 module.exports = {
-    create(req, res) {
+    create( req, res ) {
         return Todo
             .create({
                 title: req.body.title,
             })
-            .then(todo => res.status(201).send(todo))
-            .catch(error => res.status(400).send(error));
+            .then( todo => res.status(201).send(todo) )
+            .catch( error => res.status(400).send(error) );
+    },
+
+    list( req, res ) {
+        return Todo
+            .all()
+            .then( todos => res.status(200).send(todos) )
+            .catch( error => res.status(400).send(error) );
     },
 };
