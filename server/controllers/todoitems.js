4ae4b9a4b8f979043cbcdf27ed9b80437d73f6a0
Austin Wright <awguitar86@gmail.com>
2018-02-24 09:48:34 -0700

    created todoitems.js in controllers folder

---
+++ server/controllers/todoitems.js
@@ -0,0 +1,13 @@
+const TodoItem = require('../../.server/models').TodoItem;
+
+module.exports = {
+    create(req, res) {
+        return TodoItem
+            .create({
+                content: req.body.content,
+                todoId: req.params.todoId,
+            })
+            .then(todoItem => req.status(200).send(todoItem))
+            .catch(error => res.status(400).send(error));
+    },
+};
